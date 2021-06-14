b6bcbc777963de6f8a068edc790d8c3251de7838
Cody Wild <codywild@berkeley.edu>
2021-06-14 13:24:06 -0700

    Modify structure of wrappers

--- basalt_baselines/bc.py
+++ basalt_baselines/bc.py
@@ -7,11 +7,16 @@ from basalt_utils.sb3_compat.policies import SpaceFlatteningActorCriticPolicy
 from basalt_utils.sb3_compat.cnns import MAGICALCNN
 from basalt_utils.callbacks import BatchEndIntermediateRolloutEvaluator
 from stable_baselines3.common.policies import ActorCriticCnnPolicy
+from stable_baselines3.common.vec_env import DummyVecEnv
+import collections
 from imitation.algorithms.bc import BC
+import imitation.data.rollout as il_rollout
+import logging
 import torch as th
 from basalt_utils import utils
 import os
 import imitation.util.logger as imitation_logger
+from sacred.observers import FileStorageObserver
 from time import time
 
 bc_baseline = Experiment("basalt_bc_baseline")
@@ -28,11 +33,10 @@ WRAPPERS = [# Transforms continuous camera action into discrete up/down/no-chang
             # wrapper_utils.FrameSkip]
 
 
-
 @bc_baseline.config
 def default_config():
     task_name = "MineRLFindCaves-v0"
-    train_batches = 10
+    train_batches = None
     train_epochs = None
     log_interval = 1
     data_root = os.getenv('MINERL_DATA_ROOT', "/Users/cody/Code/il-representations/data/minecraft")
@@ -42,10 +46,13 @@ def default_config():
     wrappers = WRAPPERS
     save_location = "/Users/cody/Code/simple_bc_baseline/results"
     policy_path = 'trained_policy.pt'
-    use_rollout_callback = True
-    callback_batch_interval = 200
+    use_rollout_callback = False
+    callback_batch_interval = 1000
     callback_rollouts = 5
     save_videos = False
+    mode = 'train'
+    test_policy_path = None
+    test_n_rollouts = None
     batch_size = 32
     n_traj = None
     lr = 1e-4
@@ -59,7 +66,31 @@ def normal_policy_class():
     del _
 
 
-@bc_baseline.automain
+@bc_baseline.main
+def main(mode):
+    if mode == 'train':
+        train_bc()
+    if mode == 'test':
+        test_bc()
+
+
+@bc_baseline.capture
+def test_bc(task_name, data_root, wrappers, test_policy_path, test_n_rollouts):
+    data_pipeline, wrapped_env = utils.get_data_pipeline_and_env(task_name, data_root, wrappers, dummy=False)
+    vec_env = DummyVecEnv([lambda: wrapped_env])
+    policy = th.load(test_policy_path)
+    trajectories = il_rollout.generate_trajectories(policy, vec_env, il_rollout.min_episodes(test_n_rollouts))
+    stats = il_rollout.rollout_stats(trajectories)
+    stats = collections.OrderedDict([(key, stats[key])
+                                     for key in sorted(stats)])
+
+    # print it out
+    kv_message = '\n'.join(f"  {key}={value}"
+                           for key, value in stats.items())
+    logging.info(f"Evaluation stats on '{task_name}': {kv_message}")
+
+
+@bc_baseline.capture
 def train_bc(task_name, batch_size, data_root, wrappers, train_epochs, n_traj, lr,
              policy_class, train_batches, log_interval, save_location, policy_path,
              use_rollout_callback, callback_batch_interval, callback_rollouts, save_videos):
@@ -104,7 +135,6 @@ def train_bc(task_name, batch_size, data_root, wrappers, train_epochs, n_traj, l
                               lr_schedule=lambda _: 1e-4,
                               features_extractor_class=MAGICALCNN)
 
-
     imitation_logger.configure(run_save_location, ["stdout", "tensorboard"])
     if use_rollout_callback:
         callback = BatchEndIntermediateRolloutEvaluator(policy=policy,
@@ -131,5 +161,12 @@ def train_bc(task_name, batch_size, data_root, wrappers, train_epochs, n_traj, l
                      log_interval=log_interval,
                      on_batch_end=callback)
     bc_trainer.save_policy(policy_path=os.path.join(run_save_location, policy_path))
+    bc_baseline.add_artifact(os.path.join(run_save_location, policy_path))
+    bc_baseline.log_scalar(f'run_location={run_save_location}', 1)
     print("Training complete; cleaning up data pipeline!")
-    data_pipeline.close()
+    #data_pipeline.close()
+
+
+if __name__ == "__main__":
+    bc_baseline.observers.append(FileStorageObserver("sacred_results"))
+    bc_baseline.run_commandline()
