fc5a557763acc2356069ab47e5fc78b0a69167c1
Cody Wild <codywild@berkeley.edu>
2021-06-15 15:51:04 -0700

    DRLHP work

--- basalt_baselines/drlhp_baseline.py
+++ basalt_baselines/drlhp_baseline.py
@@ -5,22 +5,23 @@ import basalt_utils.wrappers as wrapper_utils
 from basalt_utils import utils
 from basalt_utils.sb3_compat.policies import SpaceFlatteningActorCriticPolicy
 from drlhp import HumanPreferencesEnvWrapper
+from stable_baselines3 import PPO
+from stable_baselines3.common.torch_layers import NatureCNN
+import minerl
 
 WRAPPERS = [# Transforms continuous camera action into discrete up/down/no-change buckets on both pitch and yaw
-            wrapper_utils.CameraDiscretizationWrapper,
+            (wrapper_utils.CameraDiscretizationWrapper, dict()),
             # Flattens a Dict action space into a Box, but retains memory of how to expand back out
-            wrapper_utils.ActionFlatteningWrapper,
+            (wrapper_utils.ActionFlatteningWrapper, dict()),
             # Pull out only the POV observation from the observation space; transpose axes for SB3 compatibility
-            utils.ExtractPOVAndTranspose,
-            # Add a time limit to the environment (only relevant for testing)
-            utils.Testing10000StepLimitWrapper,
-            wrapper_utils.FrameSkip]
+            (utils.ExtractPOVAndTranspose, dict()),
+            (wrapper_utils.FrameSkip, dict(n_repeat=4))]
 
 drlhp_baseline = Experiment("basalt_drlhp_baseline")
 
 @drlhp_baseline.config
 def default_config():
-    task_name = "FindCaves-v0"
+    task_name = "MineRLBasaltFindCave-v0"
     train_batches = 10
     train_epochs = None
     log_interval = 1
@@ -42,8 +43,19 @@ def default_config():
 def train_drlhp(task_name, batch_size, data_root, wrappers, train_epochs, n_traj, lr,
                 policy_class, train_batches, log_interval, save_location, policy_path):
     env = gym.make(task_name)
-    wrapped_env = HumanPreferencesEnvWrapper(env,
+    wrapped_env = utils.wrap_env(env, wrappers)
+    wrapped_env = HumanPreferencesEnvWrapper(wrapped_env,
                                              segment_length=100,
-                                             synthetic_preferences = False,
-                                             n_initial_training_steps = 10)
-    breakpoint()
+                                             synthetic_prefs=False,
+                                             n_initial_training_steps=10)
+    space_flattening_policy = SpaceFlatteningActorCriticPolicy(observation_space=wrapped_env.observation_space,
+                                                               action_space=wrapped_env.action_space,
+                                                               lr_schedule=lambda _: 1e100,
+                                                               env=wrapped_env,
+                                                               features_extractor_class=NatureCNN)
+    def sfp(*args, **kwargs):
+        return space_flattening_policy
+    ppo_trainer = PPO(policy=sfp,
+                      env=wrapped_env)
+    ppo_trainer.learn(500, reset_num_timesteps=False)
+
